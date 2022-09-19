db6214f71d8bdaebbe22c28c5fcde19007c178b1
harisAli <o16.haris@gmail.com>
2022-09-19 22:15:20 +0500

    testimonial added

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -234,8 +234,51 @@ btn.on('click', function(e) {
       }, 1500);
 });
 
-// $(document).ready(function(){
-//     for(i = 0; i < 35; i++) {
-//       $('.portfolio-boxes').append("<div class='col-md-3 text-center mb-4 px-sm-2 px-4'><figure class='px-2'><img src='./assets/images/portfolio-box.png' width='100%' /></figure></div>");
-//     }
-//   });
+
+
+$(document).ready(function(){
+    var galleryThumbs = new Swiper('.gallery-thumbs', {
+        effect: 'coverflow',
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: '2',
+        // loop: true,
+        // coverflowEffect: {
+        //   rotate: 50,
+        //   stretch: 0,
+        //   depth: 100,
+        //   modifier: 1,
+        //   slideShadows : true,
+        // },
+        
+        coverflowEffect: {
+            rotate: 0,
+            stretch: 0,
+            depth: 50,
+            modifier: 6,
+            slideShadows : false,
+          },
+          
+      });
+      
+      
+    var galleryTop = new Swiper('.swiper-container.testimonial', {
+        speed: 400,
+        spaceBetween: 50,
+        autoplay: {
+          delay: 3000,
+          disableOnInteraction: false,
+        },
+        // loop: true,
+        direction: 'vertical',
+        pagination: {
+          clickable: true,
+          el: '.swiper-pagination',
+          type: 'bullets',
+        },
+        thumbs: {
+            swiper: galleryThumbs
+          }
+      });
+        
+});
